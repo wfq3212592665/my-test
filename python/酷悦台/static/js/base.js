37e5e5414b5e30a2356df163614dd2fbb8937700
wfq3212592665 <3212592665@qq.com>
2021-06-06 13:23:04 +0800

    导航栏

---
+++ python/酷悦台/static/js/base.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var aInput = document.getElementById("search")
+    aInput.onkeydown = function(e){
+        if (e.keycode == 13){
+            // 按下enter键
+        }
+    }
+}
